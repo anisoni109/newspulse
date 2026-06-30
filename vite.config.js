4a4d0f83d0eaa618a6fc68208ca6dd07b4c07b52
anisoni109 <anisoni109@users.noreply.github.com>
2026-07-01 01:51:39 +0200

    Add deploy scripts + GH pages base configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/newspulse/',
+  base: command === 'build' ? '/newspulse/' : '/',
   server: {
     port: 5173,
     proxy: {
@@ -21,4 +21,4 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   }
-})
+}))
